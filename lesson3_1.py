05033f06c383b10cede32c024ccf8ed50cb8978d
Lainie888 <44189101+Lainie888@users.noreply.github.com>
2023-08-03 11:48:53 +0000

    modify

---
+++ lesson3_1.py
@@ -0,0 +1,6 @@
+a=17
+if a > 18:
+    print("您的age:")
+    print("18")
+
+print("程式結束")
--- lesson4_1.py
+++ lesson4_1.py
@@ -1,14 +1,11 @@
-# a=17
-# if a > 18:
-#     print("您的age:")
-#     print("18")
-
-# print("程式結束")
 
 score = int(input("請輸入學生分數(最高分300分):"))
 is_add = input("學生是否符合加分條件?(y,n)")
 
 if (is_add == "y"):
     score *= 1.05
+    if score > 300:
+     score = 300
 
-print(f"學生分數是{round(score)}")
+
+print(f"學生分數是{round(score)}") #round 可四捨五入
