7e197fd6162c56daf0eceacf2ded4772dcfb6416
Lainie888 <44189101+Lainie888@users.noreply.github.com>
2023-08-03 12:25:25 +0000

    modify

--- lesson4_1.py
+++ lesson4_1.py
@@ -1,20 +1,34 @@
+try:
+    score = int(input("請輸入學生分數(最高分300分):"))
+    if score <=300:
+        is_add = input("學生是否符合加分條件?(y,n)")
 
-score = int(input("請輸入學生分數(最高分300分):"))
-if score <=300:
-    is_add = input("學生是否符合加分條件?(y,n)")
+        if (is_add == "y"):
+            score *= 1.05
+            if score > 300:
+                score = 300
 
-    if (is_add == "y"):
-        score *= 1.05
-        if score > 300:
-            score = 300
+        print(f"學生分數是{round(score)}") #round 可四捨五入
 
-    print(f"學生分數是{round(score)}") #round 可四捨五入
+    else:
+        print("學生分數不可大於300分")
+except:
+    print("格式錯誤")
 
-else:
-    print("學生分數不可大於300分")
+
+
 
 # score = int(input("請輸入學生分數(0~100):"))
 # if score >= 60:
 #     print("及格")
 # else:
 #     print("不及格")
+
+
+
+#專門在檢查輸入格式錯誤
+# try:
+#     money=int(input("請輸入金額:"))
+#     print(money)
+# except:
+#     print("輸入金額錯誤")
