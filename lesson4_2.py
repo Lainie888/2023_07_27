8d81e3a9ecc68970decd49846acce56f5eb7a28d
Lainie888 <44189101+Lainie888@users.noreply.github.com>
2023-08-03 13:30:50 +0000

    modify

--- lesson4_2.py
+++ lesson4_2.py
@@ -7,17 +7,48 @@
 
 #if條件判斷式 有()跟沒括號沒差，但沒括號時要空一格
 
-score = int(input("請輸入學生分數:"))
+# score = int(input("請輸入學生分數:"))
 
-if score >= 90:
-    grade = "優"
-elif score >= 80:
-    grade = "甲"
-elif score >= 70:
-    grade = "乙"
-elif score >= 60:
-    grade = "丙"
+# #多項選擇
+
+# if score >= 90:
+#     grade = "優"
+# elif score >= 80:
+#     grade = "甲"
+# elif score >= 70:
+#     grade = "乙"
+# elif score >= 60:
+#     grade = "丙"
+# else:
+#     grade = "丁"
+
+# print(f"學生等級:{grade}")
+
+# money = int(input("請輸入購買金額:"))
+# payMoney = 0
+# if(money >= 100000):
+#     payMoney = money * 0.8
+# elif(money >= 50000):
+#     payMoney = money * 0.85
+# elif(money >= 30000):
+#     payMoney = money * 0.9
+# elif(money <= 10000):
+#     payMoney = money * 0.95
+# else:
+#     payMoney = money 
+# print(f"實付金額:{int(payMoney)}") #int 是轉成整數
+
+money = int(input("請輸入購買金額:"))
+payMoney = 0
+if(money < 10000):
+    payMoney = money 
+elif(money < 30000):
+    payMoney = money * 0.95
+elif(money < 50000):
+    payMoney = money * 0.9
+elif(money < 100000):
+    payMoney = money * 0.85
 else:
-    grade = "丁"
-
-print(f"學生等級:{grade}")
+    payMoney = money * 0.8
+     
+print(f"實付金額:{int(payMoney)}") 
