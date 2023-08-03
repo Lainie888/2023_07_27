eb3ecd96fdf838e3d17eaf3cc78fc1e221e86a7e
Lainie888 <44189101+Lainie888@users.noreply.github.com>
2023-08-03 12:08:20 +0000

    modify

--- lesson4_1.py
+++ lesson4_1.py
@@ -1,11 +1,20 @@
 
 score = int(input("請輸入學生分數(最高分300分):"))
-is_add = input("學生是否符合加分條件?(y,n)")
+if score <=300:
+    is_add = input("學生是否符合加分條件?(y,n)")
 
-if (is_add == "y"):
-    score *= 1.05
-    if score > 300:
-     score = 300
+    if (is_add == "y"):
+        score *= 1.05
+        if score > 300:
+            score = 300
 
+    print(f"學生分數是{round(score)}") #round 可四捨五入
 
-print(f"學生分數是{round(score)}") #round 可四捨五入
+else:
+    print("學生分數不可大於300分")
+
+# score = int(input("請輸入學生分數(0~100):"))
+# if score >= 60:
+#     print("及格")
+# else:
+#     print("不及格")
