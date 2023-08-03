bf357ac3923c24a4ca3510a0273c74d797ed3cd3
Lainie888 <44189101+Lainie888@users.noreply.github.com>
2023-08-03 12:55:03 +0000

    modify

---
+++ lesson4_2.py
@@ -0,0 +1,26 @@
+
+# 90(含)~100分為「優」
+# 80(含)~89分為「甲」
+# 70(含)~79分為「乙」
+# 60(含)~69分為「丙」
+# 0(含)~59分為「丁」
+
+#if條件判斷式 有()跟沒括號沒差，但沒括號時要空一格
+
+score = int(input("請輸入學生分數:"))
+
+if score >= 90:
+    grade = "優"
+else:
+    if score >= 80:
+        grade = "甲"
+    else:
+        if score >= 70:
+            grade = "乙"
+        else:
+            if score >= 60:
+                grade = "丙"
+            else:
+                grade = "丁"
+                
+print(f"學生等級:{grade}")
